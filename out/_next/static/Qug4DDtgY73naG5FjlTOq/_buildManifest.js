aa7b50db6571b2715a74f4e0f77ee38b99aada22
mart1d4 <mart1danton@gmail.com>
2023-06-22 23:38:46 +0200

    Remove out dir Update README accordingly Remove unnecessary npm packages Change package keywords

--- out/_next/static/Qug4DDtgY73naG5FjlTOq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9b3afa7ca2f84e77.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
